a1d40bde25519d1a3706f192d177045a0769d76b
thehambeard <61787444+thehambeard@users.noreply.github.com>
2024-10-27 04:30:56 -0400

    Improved Post Processing Panel

--- src/UI/Panels/PostProcessingPanel.cs
+++ src/UI/Panels/PostProcessingPanel.cs
@@ -1,11 +1,112 @@
-﻿using UnityExplorer.CacheObject;
-using UnityExplorer.Inspectors;
+﻿using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityExplorer.ObjectExplorer;
 using UniverseLib.UI;
 using UniverseLib.UI.Models;
-using UniverseLib.UI.ObjectPool;
 
 namespace UnityExplorer.UI.Panels
 {
+    internal class RowComponentGroup
+    {
+        public readonly ButtonRef InspectButton;
+        public readonly ButtonRef DefaultButton;
+        public readonly Toggle Toggle;
+
+        public RowComponentGroup(ButtonRef inspectButton, ButtonRef defaultButton, Toggle toggle)
+        {
+            InspectButton = inspectButton;
+            DefaultButton = defaultButton;
+            Toggle = toggle;
+        }
+    }
+
+    internal class VolumeComponentData
+    {
+        public readonly VolumeComponent VolumeComponent;
+        public readonly VolumeData Owner;
+        public readonly bool IsActiveDefault;
+        public readonly RowComponentGroup RowComponentGroup;
+
+        public VolumeComponentData(VolumeComponent volumeComponent, VolumeData owner, RowComponentGroup rowComponentGroup)
+        {
+            VolumeComponent = volumeComponent;
+            Owner = owner;
+            RowComponentGroup = rowComponentGroup;
+
+            IsActiveDefault = volumeComponent.active;
+            rowComponentGroup.Toggle.isOn = volumeComponent.active;
+            rowComponentGroup.InspectButton.OnClick += OnInspectButtonClick;
+            rowComponentGroup.Toggle.onValueChanged.AddListener(OnToggleChanged);
+
+            RowComponentGroup.DefaultButton.Transform.parent.gameObject.SetActive(false);
+        }
+
+        public void RevertToDefault() => RowComponentGroup.Toggle.isOn = IsActiveDefault;
+        public void SetComponentState(bool isOn) => RowComponentGroup.Toggle.isOn = isOn;
+        private void OnToggleChanged(bool isOn) => VolumeComponent.active = isOn;
+        private void OnInspectButtonClick() => InspectorManager.InspectWithFilters(VolumeComponent, string.Empty);
+    }
+
+    internal class VolumeData
+    {
+        public List<VolumeComponentData> Components = [];
+
+        public readonly Volume Volume;
+        public readonly SceneData Owner;
+        public readonly RowComponentGroup RowComponentGroup;
+
+        public bool IsAnyActive => Components.Any(x => x.VolumeComponent.active);
+
+        public VolumeData(Volume volume, SceneData owner, RowComponentGroup rowComponentGroup)
+        {
+            Owner = owner;
+            Volume = volume;
+            RowComponentGroup = rowComponentGroup;
+
+            rowComponentGroup.InspectButton.OnClick += OnInspectClick;
+            rowComponentGroup.DefaultButton.OnClick += RevertAllComponents;
+            rowComponentGroup.Toggle.onValueChanged.AddListener(OnToggleChanged);
+        }
+
+        public void RevertAllComponents()
+        {
+            Components.ForEach(x => x.RevertToDefault());
+            RowComponentGroup.Toggle.SetIsOnWithoutNotify(IsAnyActive);
+        }
+
+        public void SetAllComponents(bool isOn) => RowComponentGroup.Toggle.isOn = isOn;
+        private void OnToggleChanged(bool isOn) => Components.ForEach(x => x.SetComponentState(isOn));
+        private void OnInspectClick() => InspectorManager.InspectWithFilters(Volume, string.Empty);
+    }
+
+    internal class SceneData
+    {
+        public List<VolumeData> Volumes = [];
+
+        public readonly Scene Scene;
+        public readonly RowComponentGroup RowComponentGroup;
+
+        public bool IsAnyActive => Volumes.Any(x => x.IsAnyActive);
+
+        public SceneData(Scene scene, RowComponentGroup rowComponentGroup)
+        {
+            Scene = scene;
+            RowComponentGroup = rowComponentGroup;
+            rowComponentGroup.InspectButton.OnClick += OnInspectButtonClick;
+            rowComponentGroup.DefaultButton.OnClick += RevertAllVolumes;
+            rowComponentGroup.Toggle.onValueChanged.AddListener(OnToggleChanged);
+        }
+
+        public void RevertAllVolumes()
+        {
+            Volumes.ForEach(x => x.RevertAllComponents());
+            RowComponentGroup.Toggle.SetIsOnWithoutNotify(IsAnyActive);
+        }
+
+        private void OnToggleChanged(bool isOn) => Volumes.ForEach(x => x.SetAllComponents(isOn));
+        private void OnInspectButtonClick() => InspectorManager.InspectWithFilters(Scene, string.Empty);
+    }
+
     internal class PostProcessingPanel(UIBase owner) : UEPanel(owner)
     {
         public override string Name => "Post-processing";
@@ -16,199 +117,66 @@ namespace UnityExplorer.UI.Panels
         public override Vector2 DefaultAnchorMax => new(0.6f, 0.6f);
         public override bool NavButtonWanted => true;
         public override bool ShouldSaveActiveState => true;
-        public static bool FoundAnyEffect => postProcessingEffects != null && postProcessingEffects.Count > 0;
+        public readonly List<SceneData> SceneData = [];
 
-        public static Dictionary<string, List<PPEffect>> postProcessingEffects = null;
-        static ButtonRef updateEffects;
-        List<GameObject> UIElements = new List<GameObject>();
-
-
-        public class PPEffect
-        {
-            public PPEffect(object obj)
-            {
-                Object = obj;
-
-                ReflectionInspector inspector = Pool<ReflectionInspector>.Borrow();
-                inspector.Target = obj.TryCast();
-                //ReflectionInspector inspector = InspectorManager.CreateInspectorWithoutWindow<ReflectionInspector>(obj.TryCast(), false, null);//Pool<ReflectionInspector>.Borrow();//
-
-                Type objType = obj is Type type ? type : obj.GetActualType();
-                List<CacheMember> members = CacheMemberFactory.GetCacheMembers(objType, inspector);
-
-                foreach (CacheMember member in members)
-                {
-                    if (member.NameForFiltering.EndsWith(".active"))
-                    {
-                        member.Evaluate();
-                        Active = member;
-                    }
-
-                    if (member.NameForFiltering.EndsWith(".name"))
-                    {
-                        member.Evaluate();
-                        Name = member;
-                    }
-                }
-            }
-
-            public object Object { get; }
-            public CacheMember Active { get; }
-            public CacheMember Name { get; }
-
-            public override string ToString() => $"{Object}";
-        }
+        private GameObject _content;
+        private ButtonRef _updateEffects;
+        private readonly List<GameObject> _uiElements = [];
 
         public void UpdatePPElements()
         {
-            if (postProcessingEffects != null)
-            {
-                // We turn the effects we had back on so they get captured again on refresh
-                foreach (List<PPEffect> effects in postProcessingEffects.Values)
-                {
-                    SetEffect(true, effects);
-                }
-                postProcessingEffects.Clear();
-            }
+            DestroyScrollViewContent();
 
-            postProcessingEffects = new Dictionary<string, List<PPEffect>>();
+            SceneData.Clear();
 
+            SceneHandler.Update();
 
-            try
-            {
-                AddEffect("Owlcat.Runtime.Visual.RenderPipeline.PostProcess.HBAO", "Hbao");
-            }
-            catch { }
-
-            string[] volumeClassEffects = {
-                "Bloom",
-                "BloomEnhanced",
-                "ChannelMixer",
-                "ChromaticAberration",
-                "ColorCurves",
-                "ColorLookup",
-                "Daltonization",
-                "DepthOfField",
-                "FilmGrain",
-                "LensDistortion",
-                "LiftGammaGain",
-                "MaskedColorTrqnsform",
-                "MotionBlur",
-                "PaniniProjection",
-                "RadialBlur",
-                "SaturationOverlay",
-                "ScreenSpaceCloudShadows",
-                "ScreenSpaceReflections",
-                "Sepia",
-                "ShadowsMidtonesHighlights",
-                "SlopePowerOffset",
-                "SplitToning",
-                "ToneMapping",
-                "Vignette",
-                "WhiteBalance"
-            };
-
-            foreach (string effect in volumeClassEffects)
+            foreach (Scene scene in SceneHandler.LoadedScenes
+                .Where(s => !string.IsNullOrEmpty(s.name) && s.isLoaded))
             {
                 try
                 {
-                    AddEffect("Owlcat.Runtime.Visual.RenderPipeline.PostProcess", effect);
-                }
-                catch { }
-            }
+                    var volumes = scene.GetRootGameObjects()
+                       .SelectMany(rootObject => rootObject.GetComponentsInChildren<Volume>(includeInactive: false))
+                       .ToList();
 
-            if (!FoundAnyEffect)
-            {
-                ExplorerCore.Log("Couldn't find any standard post-processing effect classes.");
-            }
+                    if (volumes.Count == 0)
+                        continue;
 
-            BuildEffectTogglers();
-        }
+                    SceneData sData = new(scene, ConstructRow(scene.name));
 
-        private void AddEffect(string baseClass, string effect)
-        {
-            try
-            {
-                Type searchType = ReflectionUtility.GetTypeByName($"{baseClass}.{effect}");
-
-                if (searchType == null) return;
-
-                searchType = searchType is Type type ? type : searchType.GetActualType();
-                List<object> currentResults = RuntimeHelper.FindObjectsOfTypeAll(searchType).Select(obj => (object)obj).ToList();
-
-                foreach (object obj in currentResults)
-                {
-                    PPEffect entry = new PPEffect(obj);
-
-                    // Ignore non-active objects and objects without a name, since those tend to be irrelevant
-                    if ((bool)entry.Active.Value && entry.Name.Value.ToString() != "")
+                    for (int i = 0; i < volumes.Count; i++)
                     {
-                        if (!postProcessingEffects.ContainsKey(effect))
-                        {
-                            postProcessingEffects.Add(effect, new List<PPEffect>());
-                        }
+                        if (volumes[i].profile.components.Count == 0)
+                            continue;
 
-                        postProcessingEffects[effect].Add(entry);
+                        sData.Volumes.Add(new(
+                            volumes[i],
+                            sData,
+                            ConstructRow(UnityHelpers.GetTransformPath(volumes[i].transform, true), 1)));
+
+                        foreach (var comp in volumes[i].profile.components)
+                            sData.Volumes[i].Components.Add(new(comp, sData.Volumes[i], ConstructRow(comp.name, 2)));
                     }
+
+                    SceneData.Add(sData);
                 }
-            }
-            catch
-            {
-                // ExplorerCore.Log(ex.Message);
-            }
-        }
-
-        private void SetEffect(bool value, List<PPEffect> effects)
-        {
-            foreach (PPEffect obj in effects)
-            {
-                obj.Active.TrySetUserValue(value);
-            }
-        }
-
-        private void BuildEffectTogglers()
-        {
-            foreach (var comp in UIElements)
-            {
-                UnityEngine.Object.Destroy(comp);
-                //UIElements.Remove(comp);
-            }
-
-            foreach (string effect in postProcessingEffects.Keys)
-            {
-                GameObject horiGroup = UIFactory.CreateHorizontalGroup(ContentRoot, $"{effect}_Group", false, false, true, false, 4,
-                    default, new Color(1, 1, 1, 0), TextAnchor.MiddleLeft);
-                UIFactory.SetLayoutElement(horiGroup, minHeight: 25, flexibleWidth: 9999);
-                UIElements.Add(horiGroup);
-
-                //Active toggle
-                Toggle groupEffectToggle = new Toggle();
-                GameObject toggleEffect = UIFactory.CreateToggle(horiGroup, $"Toggle {effect}", out groupEffectToggle, out Text effectToggleText);
-                UIFactory.SetLayoutElement(toggleEffect, minHeight: 25);
-                groupEffectToggle.onValueChanged.AddListener(value => SetEffect(value, postProcessingEffects[effect]));
-                groupEffectToggle.isOn = true; // we picked up only the active effects
-                effectToggleText.text = effect;
-
-                GameObject buttonsGroup = UIFactory.CreateHorizontalGroup(horiGroup, $"{effect}_Group", false, false, true, false, 4,
-                default, new Color(1, 1, 1, 0), TextAnchor.MiddleRight);
-                UIFactory.SetLayoutElement(buttonsGroup, minHeight: 25, flexibleWidth: 9999);
-                UIElements.Add(buttonsGroup);
-
-                for (int i = 0; i < postProcessingEffects[effect].Count; i++)
+                catch(Exception ex) 
                 {
-                    PPEffect obj = postProcessingEffects[effect][i];
-
-                    ButtonRef openEffect = UIFactory.CreateButton(buttonsGroup, $"Inspect {obj.Object}", $"Obj{i}");
-                    UIFactory.SetLayoutElement(openEffect.GameObject, minHeight: 25, minWidth: 40);
-                    openEffect.OnClick += () => InspectorManager.InspectWithFilters(obj.Object, effect,
-                        UnityExplorer.Inspectors.MemberFilter.Property |
-                        UnityExplorer.Inspectors.MemberFilter.Field
-                    );
+                    ExplorerCore.LogError($"Failed creating SceneData for Scene {scene}");
+                    ExplorerCore.LogError(ex.Message + ex.StackTrace);
                 }
             }
         }
+
 
         // ~~~~~~~~ UI construction / callbacks ~~~~~~~~
+
+        private void DestroyScrollViewContent()
+        {
+            _uiElements.ForEach(e => GameObject.DestroyImmediate(e));
+            _uiElements.Clear();
+        }
 
         protected override void ConstructPanelContent()
         {
@@ -217,12 +185,58 @@ namespace UnityExplorer.UI.Panels
             UIFactory.SetLayoutElement(horiGroup, minHeight: 25, flexibleWidth: 9999);
             //UIElements.Add(horiGroup);
 
-            updateEffects = UIFactory.CreateButton(horiGroup, "RefreshEffects", "Refresh Effects");
-            UIFactory.SetLayoutElement(updateEffects.GameObject, minWidth: 150, minHeight: 25);
-            updateEffects.OnClick += UpdatePPElements;
+            _updateEffects = UIFactory.CreateButton(horiGroup, "RefreshEffects", "Refresh Effects");
+            UIFactory.SetLayoutElement(_updateEffects.GameObject, minWidth: 150, minHeight: 25);
+            _updateEffects.OnClick += UpdatePPElements;
 
             Text openComponentLabel = UIFactory.CreateLabel(horiGroup, "OpenComponentLabel", "Open object in inspector  ", TextAnchor.MiddleRight, Color.white, false, 15);
             UIFactory.SetLayoutElement(openComponentLabel.gameObject, minHeight: 25, minWidth: 60, flexibleWidth: 9999);
+
+            GameObject scrollView = UIFactory.CreateScrollView(ContentRoot, "VolumeScrollView", out _content, out _, new Color(0.11f, 0.11f, 0.11f));
+            UIFactory.SetLayoutElement(scrollView, flexibleHeight: 9999);
+        }
+
+        private void ConstructSpacer(GameObject partent, int? width)
+        {
+            GameObject spacerObj = UIFactory.CreateUIObject("Spacer", partent, new Vector2(0, 0));
+            UIFactory.SetLayoutElement(spacerObj, minWidth: width, flexibleWidth: 0, minHeight: 0, flexibleHeight: 0);
+        }
+
+        private RowComponentGroup ConstructRow(string text, int tab = 0)
+        {
+            GameObject horiGroup = UIFactory.CreateHorizontalGroup(_content, "SceneRow", false, false, true, true, 3,
+                default, new Color(1, 1, 1, 0), TextAnchor.MiddleLeft);
+            UIFactory.SetLayoutElement(horiGroup, minHeight: 25, flexibleWidth: 9999);
+
+            ConstructSpacer(horiGroup, 10 * tab);
+            
+            GameObject toggleObj = UIFactory.CreateToggle(horiGroup, "BehaviourToggle", out Toggle enabledToggle, out Text behavText, default, 17, 17);
+            UIFactory.SetLayoutElement(toggleObj, minHeight: 17, flexibleHeight: 0, minWidth: 17);
+
+            GameObject defaultBtnHolder = UIFactory.CreateHorizontalGroup(horiGroup, "NameButtonHolder",
+                false, false, true, true, childAlignment: TextAnchor.MiddleLeft);
+            UIFactory.SetLayoutElement(defaultBtnHolder, minWidth: 50, minHeight: 25, flexibleHeight: 0);
+            defaultBtnHolder.AddComponent<Mask>().showMaskGraphic = false;
+
+            var defaultButton = UIFactory.CreateButton(defaultBtnHolder, "DefaultButton", "Default", new Color(0.20f, 0.20f, 0.20f));
+            UIFactory.SetLayoutElement(defaultButton.Component.gameObject, minWidth: 50, minHeight: 25, flexibleHeight: 0);
+
+            GameObject nameBtnHolder = UIFactory.CreateHorizontalGroup(horiGroup, "NameButtonHolder",
+                false, false, true, true, childAlignment: TextAnchor.MiddleLeft);
+            UIFactory.SetLayoutElement(nameBtnHolder, flexibleWidth: 9999, minHeight: 25, flexibleHeight: 0);
+            nameBtnHolder.AddComponent<Mask>().showMaskGraphic = false;
+
+            var nameButton = UIFactory.CreateButton(nameBtnHolder, "NameButton", "Name", new Color(0.11f, 0.11f, 0.11f));
+            UIFactory.SetLayoutElement(nameButton.Component.gameObject, flexibleWidth: 9999, minHeight: 25, flexibleHeight: 0);
+
+            Text nameLabel = nameButton.Component.GetComponentInChildren<Text>();
+            nameLabel.horizontalOverflow = HorizontalWrapMode.Overflow;
+            nameLabel.alignment = TextAnchor.MiddleLeft;
+            nameLabel.text = text;
+
+            _uiElements.Add(horiGroup);
+
+            return new(nameButton, defaultButton, enabledToggle);
         }
     }
 }
